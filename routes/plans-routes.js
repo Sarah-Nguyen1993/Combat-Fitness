b3a4501289d6a98633cee92f1a1aed95f500234d
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-11-04 13:52:15 -0600

    created api routes for axios api data

---
+++ routes/plans-routes.js
@@ -0,0 +1,36 @@
+const axios = require("axios");
+require("dotenv").config();
+
+module.exports = function (app) {
+
+    app.get("/api/bulk", function (req, res) {
+
+        axios.get("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=relevance&q=workout%20plans%20for%20bulking%20up%20&type=video&key=" + process.env.API_KEY)
+            .then(response => {
+                res.json(response.data.items);
+            }).catch(err => {
+                console.log(err);
+            });
+    });
+
+    app.get("/api/lean", function (req, res) {
+
+        axios.get("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=relevance&q=workout%20plans%20to%20get%20lean&type=video&key=" + process.env.API_KEY)
+        .then(response => {
+            res.json(response.data.items);
+        }).catch(err => {
+            console.log(err);
+        });
+    });
+
+    app.get("/api/hiit", function (req, res) {
+
+        axios.get("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=relevance&q=hiit%20workouts&type=video&key=" + process.env.API_KEY)
+        .then(response => {
+            res.json(response.data.items);
+        }).catch(err => {
+            console.log(err);
+        });
+    });
+
+};
