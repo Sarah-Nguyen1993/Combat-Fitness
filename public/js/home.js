ce805ab9014ff48d8f45c1c8bf033811d1a802cb
Matthew Oballe <oballematt@gmail.com>
2020-11-04 13:08:33 -0600

    checking buttons, fixing syntax

--- public/js/home.js
+++ public/js/home.js
@@ -1,36 +1,36 @@
-$(document).ready(function() {
-  
+$(document).ready(function () {
+    
+    $("#calculate").on("click", function () {
+        calories()
+    })
+
+    $("#goalsave").on("click", function(){
+        goals()
+
+    })
 
 
+    function calories() {
+        const gender = $(".gender").val()
+        const age = $(".age").val()
+        const height = $(".height").val()
+        const weight = $(".weight").val()
+        const activity = $("#inputGroupSelect01").val()
+        const goal = $("#inputGroupSelect02").val()
+        const rate = $("#inputGroupSelect03").val()
 
 
-$("#calculate").on("click", function(){
-    calories()
+        $("div").removeClass("hide")
+        console.log(gender, age, height, weight, activity, goal, rate)
+    }
 })
 
-
-function calories(){
-    const gender = $(".gender").val()
-    const age = $(".age").val()
-    const height = $(".height").val()
-    const weight = $(".weight").val()
-    const activity = $("#inputGroupSelect01").val()
-    const goal = $("#inputGroupSelect02").val()
-    const rate = $("#inputGroupSelect03").val()
-
-
-    $("div").removeClass("hide")
-    console.log(gender, age, height, weight, activity, goal, rate)
-}
-
-function display(){
-
-
-
+function goals(){
+    const goal = $("#goaltext").val()
+    console.log(goal)
 }
 
 
-})
 
 
 
