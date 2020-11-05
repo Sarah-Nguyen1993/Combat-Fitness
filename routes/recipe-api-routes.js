f9361fe8d5bd0736d7617aac4507ac99fa24ba22
Dung Nguyen <dudu.nt107@gmail.com>
2020-11-04 20:52:25 -0600

    fixed some codes

--- routes/recipe-api-routes.js
+++ routes/recipe-api-routes.js
@@ -1,36 +1,34 @@
 const db = require("../models");
-module.export = function(app){
+module.exports = function(app){
     app.get("/api/recipes", function(req,res){
         db.Recipe.findAll({
             include:[db.User]
         }).then(function(dbRecipe){
             res.json(dbRecipe)
+        }).catch(err => {
+            console.log(err);
+            res.status(500).end();
         });
     });
-    app.post("/api/recipes", function(req,res){
-        db.Recipe.findAll({
-            include:[db.User]
-        }).then(dbRecipe => {
-            res.json(dbRecipe)
-        });
-    });
-    app.post("api/recipes/:id", function(req,res){
+
+    app.post("api/recipes", function(req,res){
         db.Recipe.create({
-            where: {
-                id:req.params.id
-            }
+            title: req.body.title, 
+            prep_time: req.body.prep_time, 
+            servings: req.body.servings,
+            sourceUrl: req.body.sourceUrl
         }).then(dbRecipe => {
             res.json(dbRecipe)
         })
     })
 
-    app.delete("/api/recipes/:id", function(req,res){
-        db.Recipe.destroy({
-            where: {
-                id: req.params.id
-            }
-        }).then(dbRecipe => {
-            res.json(dbRecipe)
-        })
-    })
+    // app.delete("/api/recipes/:id", function(req,res){
+    //     db.Recipe.destroy({
+    //         where: {
+    //             id: req.params.id
+    //         }
+    //     }).then(dbRecipe => {
+    //         res.json(dbRecipe)
+    //     })
+    // })
 }
